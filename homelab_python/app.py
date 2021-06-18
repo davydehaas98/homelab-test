6812a7b449cf118da1bd0e2721285fa44fbb0be5
Davy de Haas <davy.dehaas98@gmail.com>
2021-06-18 22:00:03 +0200

    Remove dotenv

--- homelab_python/app.py
+++ homelab_python/app.py
@@ -1,9 +1,7 @@
-from dotenv import load_dotenv
 from homelab_python.scripts.am2320 import *
 
 
 def run():
-    load_dotenv()
     while True:
         am2320_temperature()
         am2320_humidity()

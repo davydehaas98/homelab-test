b318f87157ab56bb2a9d4917ea98762306686a59
Davy de Haas <davy.dehaas98@gmail.com>
2021-06-19 13:27:10 +0200

    Add Fahrenheit

--- homelab_python/scripts/am2320.py
+++ homelab_python/scripts/am2320.py
@@ -7,9 +7,14 @@ am2320 = adafruit_am2320.AM2320(i2c);
 
 def am2320_temperature():
     now = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
-    print(F"{now} | Temperature: {am2320.temperature} °C")
+    temperature_c = am2320.temperature
+    temperature_f = temperature_c * (9/5) + 32
+    
+    print(F"{now} | Temperature: {temperature_c} °C")
+    print(F"{now} | Temperature: {temperature_f} °F")
 
 
 def am2320_humidity():
     now = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
+    
     print(F"{now} | Humidity: {am2320.relative_humidity} %")
